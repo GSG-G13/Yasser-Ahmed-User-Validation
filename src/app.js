85a0e0bad1f4f55409eaa49712f6b59da70ab40c
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-03-29 12:09:02 +0300

    handler html css

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes/routes');
+// const routes = require('./routes/routes');
 const compression = require('compression');
 const path = require('path');
 
@@ -8,9 +8,9 @@ const app = express();
 app.disable('x-powered-by');
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
-app.use(express.static(path.join(__dirname,'..','public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 module.exports = app;
