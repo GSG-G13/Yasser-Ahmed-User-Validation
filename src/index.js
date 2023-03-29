ac80fb788f7d87a6799c0e493f0c306108d78eab
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-03-29 10:57:31 +0300

    Edite File of Listing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ const app = require('./app.js');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`The things, the things are happening on ${port}!!`);
-    console.log('You can also do this... to show the port %d', port);
+    console.log(`server is Running => http://localhost:${port}}`);
 });
