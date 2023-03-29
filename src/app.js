2ed655698e5c94f6e68827561ac98ff863f307ab
AhmedMughessib <amoghaseeb@gmail.com>
2023-03-29 12:06:43 +0300

    add middlewares to the server

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const compression = require('compression');
+const path = require('path');
 
 const app = express();
 
+app.disable('x-powered-by');
+app.use(express.urlencoded({ extended: false }));
+app.use(compression());
+app.use(express.static(path.join(__dirname,'..','public')));
 
 app.use(express.json());
 app.use(routes);
