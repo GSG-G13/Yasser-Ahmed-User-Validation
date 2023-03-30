fd375e8f78cbc8bf5708812111e05191dac75d8a
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-03-30 10:42:16 +0300

    edite dom Fininsh

--- public/dom.js
+++ public/dom.js
@@ -3,36 +3,46 @@ const emailInput = document.querySelector(".emailInput");
 
 
 SubmitI.addEventListener('click', () => {
-    fetch(`/submit:${emailInput.value}`, {
-    }).then(result => result.json())
+    fetch(`/submit`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email: `${emailInput.value}` })
+    })
+        .then(result => result.json())
         .then(result => showResult(result));
 });
 
 const showResult = (data) => {
-    if (data.valid === false || data.desposable === true){
+    console.log(data);
+    if (data.valid === false) {
         console.log(data);
         const massageElement = document.createElement("p");
-        const massageText = document.createTextNode(`Email is invalid!`);
+        const massageText = document.createTextNode(`Email is invalid! try  again`);
         massageElement.appendChild(massageText);
         massageElement.classList.add('massageElement')
         const originalElement = document.getElementById('originalElement');
-        originalElement.setAttribute('class','newClass')
+        originalElement.setAttribute('class', 'newClass')
         console.log(massageElement);
         console.log(originalElement);
         const parentElement = document.getElementById('parentElement')
         parentElement.appendChild(massageElement);
 
 
-    } else{
-        console.log('here the user will be directed to the new page with masage ');
+
+    } else {
+        const massageElement = document.createElement("p");
+        const massageText = document.createTextNode(`Email is valid!`);
+        massageElement.appendChild(massageText);
+        massageElement.classList.add('successEle')
+        const originalElement = document.getElementById('originalElement');
+        originalElement.setAttribute('class', 'success')
+        console.log(massageElement);
+        console.log(originalElement);
+        const parentElement = document.getElementById('parentElement')
+        parentElement.appendChild(massageElement);
     }
-    
-    
-    
-    // console.log(data);
-    // if (data.valid === false) {
-    //     console.log("hi");
-    // } else {
-    //     console.log('its working');
-    // }
+
+
 }
